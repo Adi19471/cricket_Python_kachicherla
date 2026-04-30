fe7c5f3feb12181b61a31e81c27e32709f60ea17
Adi19471 <akumatha@gmail.com>
2026-04-30 16:37:41 +0530

    admin kcp

--- cricket_booking/urls.py
+++ cricket_booking/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from booking import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('kcb/', admin.site.urls),
     path('', include('booking.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
